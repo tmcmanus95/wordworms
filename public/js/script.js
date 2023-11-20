ccdf88017421139517d8142088f04e605946031a
tmcmanus95 <tmcmanus95@gmail.com>
2023-11-20 16:59:32 -0600

    initial commit

---
+++ public/js/script.js
@@ -0,0 +1,339 @@
+const numRows = 8;
+const numCols = 8;
+const gridContainer = document.querySelector("#grid-container");
+const pastWordsDisplay = document.querySelector("#past-words-display");
+const submitButton = document.querySelector("#submit-button");
+const testSubmitButton = document.querySelector("#test-submit-button");
+const currentWordDisplay = document.querySelector("#current-word-display");
+const wordStatusDisplay = document.querySelector("#word-status-display");
+const scoreDisplay = document.querySelector("#score-display");
+let selectedTiles = [];
+let score = 0;
+let currentWord = "";
+let englishWords;
+let pastCorrectWords = [];
+
+let lastSelectedIndex = null;
+
+// Fetches the dictionary
+fetch("../../words_dictionary.json")
+  .then((response) => response.json())
+  .then((data) => {
+    englishWords = data;
+
+    createGrid();
+  })
+  .catch((error) => console.error("Error fetching words dictionary:", error));
+
+function createGrid() {
+  for (let row = 0; row < numRows; row++) {
+    for (let col = 0; col < numCols; col++) {
+      const gridItem = document.createElement("div");
+      gridItem.className = "grid-item";
+      gridItem.textContent = getRandomLetter();
+      let coordinates = `${row},${col}`;
+      gridItem.id = coordinates;
+      gridItem.addEventListener("click", () => toggleSelection(row, col));
+      gridContainer.appendChild(gridItem);
+    }
+  }
+}
+
+function countColumnInstances(selectedTiles) {
+  const uniqueRows = new Set();
+  const uniqueColumns = new Set();
+
+  selectedTiles.forEach((tile) => {
+    const { row, col } = tile;
+    uniqueRows.add(row);
+    uniqueColumns.add(col);
+  });
+
+  const rowInstances = Array.from(uniqueRows).map((row) => ({
+    row: parseInt(row),
+    instances: 1, // You can set instances to 1 or count occurrences if needed
+  }));
+
+  const columnInstances = Array.from(uniqueColumns).map((col) => ({
+    column: parseInt(col),
+    instances: 1, // You can set instances to 1 or count occurrences if needed
+  }));
+
+  console.log("Here are my row counts: ", rowInstances);
+  console.log("Here are my column counts: ", columnInstances);
+
+  return columnInstances, rowInstances;
+}
+
+function toggleSelection(row, col) {
+  const index = row * numCols + col;
+  const gridItem = gridContainer.children[index];
+  const currentLetter = gridItem.textContent;
+  // Check if the tile is already selected
+  const isSelected = selectedTiles.some(
+    (tile) => tile.row === row && tile.col === col
+  );
+
+  if (isSelected) {
+    // If the tile is already selected, find its index and deselect it
+    const selectedIndex = selectedTiles.findIndex(
+      (tile) => tile.row === row && tile.col === col
+    );
+
+    if (selectedIndex === lastSelectedIndex) {
+      // Deselect the tile
+      selectedTiles.splice(selectedIndex, 1);
+      gridItem.classList.remove("selected");
+      currentWord = currentWord.slice(0, -1);
+
+      // Update the last selected index for the case of sequential reverse deselection
+      lastSelectedIndex =
+        selectedTiles.length > 0 ? selectedTiles.length - 1 : null;
+    }
+  } else {
+    // Check if the tile is adjacent to any selected tile
+    const isAdjacent = selectedTiles.some(
+      (tile) => Math.abs(tile.row - row) <= 1 && Math.abs(tile.col - col) <= 1
+    );
+
+    if (isAdjacent || selectedTiles.length === 0) {
+      // Select the tile and include the index
+      selectedTiles.push({
+        row,
+        col,
+        letter: gridItem.textContent,
+        index,
+      });
+      currentWord += currentLetter;
+      gridItem.classList.add("selected");
+
+      lastSelectedIndex = selectedTiles.length - 1;
+    }
+  }
+
+  console.clear();
+  console.log(selectedTiles);
+  console.log(currentWord);
+
+  currentWordDisplay.textContent = currentWord;
+  const columnInstances = countColumnInstances(selectedTiles);
+  for (i = 0; i < columnInstances.length; i++) {
+    findInstancesForColumn(columnInstances, columnInstances[i].column);
+  }
+  console.log(columnInstances);
+}
+
+function findInstancesForColumn(columnInstances, targetColumn) {
+  const targetInstance = columnInstances.find(
+    (columnInstance) => columnInstance.column === targetColumn
+  );
+  if (targetInstance.instances) {
+    console.log(
+      `Column ${targetInstance.column} has ${targetInstance.instances}`
+    );
+  }
+  return targetInstance ? targetInstance.instances : 0;
+}
+
+function clearSelectedTiles() {
+  selectedTiles.forEach((tile) => {
+    const index = tile.row * numCols + tile.col;
+    const gridItem = gridContainer.children[index];
+    gridItem.classList.remove("selected");
+  });
+
+  // selectedTiles = [];
+  currentWord = "";
+}
+
+function getRandomLetter() {
+  const letterFrequencies = {
+    A: 8.17,
+    B: 1.49,
+    C: 2.78,
+    D: 4.25,
+    E: 12.7,
+    F: 2.23,
+    G: 2.02,
+    H: 6.09,
+    I: 6.97,
+    J: 0.15,
+    K: 0.77,
+    L: 4.03,
+    M: 2.41,
+    N: 6.75,
+    O: 7.51,
+    P: 1.93,
+    Q: 0.1,
+    R: 5.99,
+    S: 6.33,
+    T: 9.06,
+    U: 2.76,
+    V: 0.98,
+    W: 2.36,
+    X: 0.15,
+    Y: 1.97,
+    Z: 0.07,
+  };
+
+  const totalFrequency = Object.values(letterFrequencies).reduce(
+    (sum, frequency) => sum + frequency,
+    0
+  );
+
+  const randomFrequency = Math.random() * totalFrequency;
+  let cumulativeFrequency = 0;
+
+  for (const [letter, frequency] of Object.entries(letterFrequencies)) {
+    cumulativeFrequency += frequency;
+    if (randomFrequency <= cumulativeFrequency) {
+      return letter;
+    }
+  }
+
+  // Fallback letter
+  return "E";
+}
+
+function randomizeLetters(selectedTiles) {
+  const uniqueRows = new Set();
+  const uniqueColumns = new Set();
+
+  selectedTiles.forEach((tile) => {
+    const { row, col } = tile;
+    uniqueRows.add(row);
+    uniqueColumns.add(col);
+  });
+
+  // Convert sets to arrays for easier iteration
+  const rowsArray = Array.from(uniqueRows);
+  const columnsArray = Array.from(uniqueColumns);
+
+  // Randomize letters in rows
+  rowsArray.forEach((row) => {
+    for (let col = 0; col < numCols; col++) {
+      const index = row * numCols + col;
+      const gridItem = gridContainer.children[index];
+      gridItem.textContent = getRandomLetter();
+      gridItem.classList.add("flip-animation");
+      gridItem.classList.add("randomized");
+    }
+  });
+
+  // Randomize letters in columns
+  columnsArray.forEach((col) => {
+    for (let row = 0; row < numRows; row++) {
+      const index = row * numCols + col;
+      const gridItem = gridContainer.children[index];
+      gridItem.textContent = getRandomLetter();
+      gridItem.classList.add("flip-animation");
+      gridItem.classList.add("randomized");
+    }
+  });
+
+  setTimeout(() => {
+    const allGridItems = document.querySelectorAll(".grid-item");
+    allGridItems.forEach((gridItem) => {
+      gridItem.classList.remove("flip-animation");
+      gridItem.classList.remove("correct");
+      gridItem.classList.remove("randomized");
+    });
+  }, 600);
+}
+
+function checkWordValidity(word) {
+  wordStatusDisplay.textContent = "";
+
+  if (word.length > 2 && englishWords.hasOwnProperty(word.toLowerCase())) {
+    console.log(`${word} is a valid English word.`);
+    wordStatusDisplay.textContent = `${word} is a valid English word.`;
+    score += 10 ** (currentWord.length - 1);
+    scoreDisplay.textContent = score;
+    console.log(score);
+    selectedTiles.forEach((tile) => {
+      const { row, col } = tile;
+      const index = row * numCols + col;
+      const gridItem = gridContainer.children[index];
+      gridItem.classList.add("correct");
+    });
+    pastCorrectWords.push(word);
+    const sortedCorectWords = pastCorrectWords.sort(
+      (a, b) => b.length - a.length
+    );
+    updatePastWordsDisplay(pastCorrectWords);
+    console.log("Past correct words: ", sortedCorectWords);
+    randomizeLetters(selectedTiles);
+  } else {
+    console.log(`${word} is not a valid English word.`);
+    selectedTiles.forEach((tile) => {
+      const { row, col } = tile;
+      const index = row * numCols + col;
+      const gridItem = gridContainer.children[index];
+      gridItem.classList.add("incorrect");
+    });
+    wordStatusDisplay.textContent = `${word} is not a valid English word.`;
+  }
+  setTimeout(() => {
+    wordStatusDisplay.textContent = "";
+    currentWordDisplay.textContent = "";
+    selectedTiles.forEach((tile) => {
+      const { row, col } = tile;
+      const index = row * numCols + col;
+      const gridItem = gridContainer.children[index];
+      gridItem.classList.remove("incorrect");
+    });
+    selectedTiles = [];
+  }, 1000);
+}
+
+function updatePastWordsDisplay(pastCorrectWords) {
+  pastWordsDisplay.textContent = "";
+  const bestTenWords = pastCorrectWords.slice(0, 10);
+  bestTenWords.forEach((word) => {
+    const correctWordDisplay = document.createElement("p");
+    correctWordDisplay.textContent =
+      word + " - " + 10 ** (word.length - 1) + " points";
+    pastWordsDisplay.appendChild(correctWordDisplay);
+  });
+}
+
+let isDragging = false;
+let startRow, startCol;
+
+gridContainer.addEventListener("touchstart", (event) => {
+  isDragging = true;
+  const { clientX, clientY } = event.touches[0];
+  const { row, col } = getRowAndColFromCoordinates(clientX, clientY);
+  startRow = row;
+  startCol = col;
+  toggleSelection(row, col);
+});
+
+gridContainer.addEventListener("touchmove", (event) => {
+  if (isDragging) {
+    event.preventDefault();
+    const { clientX, clientY } = event.touches[0];
+    const { row, col } = getRowAndColFromCoordinates(clientX, clientY);
+    if (row !== startRow || col !== startCol) {
+      toggleSelection(row, col);
+      startRow = row;
+      startCol = col;
+    }
+  }
+});
+
+gridContainer.addEventListener("touchend", () => {
+  isDragging = false;
+});
+
+function getRowAndColFromCoordinates(x, y) {
+  const rect = gridContainer.getBoundingClientRect();
+  const col = Math.floor((x - rect.left) / gridItemWidth);
+  const row = Math.floor((y - rect.top) / gridItemHeight);
+  return { row, col };
+}
+
+submitButton.addEventListener("click", function () {
+  checkWordValidity(currentWord);
+  clearSelectedTiles();
+});
---
+++ server.js
@@ -0,0 +1,11 @@
+// server.js with Express
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname)));
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
